bca25367eff7eb02c21b9308d595e4d84a992151
岡優志 <yu.j.0513@gmail.com>
2021-10-25 23:12:33 +0900

    -

---
+++ CICDTest/CICDTest/CICDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestApp.swift
+//  CICDTest
+//
+//  Created by oka yuuji on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
